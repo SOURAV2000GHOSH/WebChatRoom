cf16d1bad57c3a774764b9a2bc0bf23a1adb8b54
Sourav <souravghosh6543@gmail.com>
2024-07-28 13:15:03 +0530

    adding basic design of web-chat-room messaging application

---
+++ src/test/java/com/springboot/webchat/WebChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
